7446103e3171941d148fdd37d5adcc60af9315b6
Matt <97047080+mattxmade@users.noreply.github.com>
2022-06-26 13:15:29 +0100

    Add basic calculator unit test

--- 03-calculator/calculator.test.js
+++ 03-calculator/calculator.test.js
@@ -1,1 +1,8 @@
 const calculator = require('./calculator.js');
+
+test('Given two numbers, calculate correct number', () => { 
+  expect(calculator.add(1,2)).toEqaul(3);
+  expect(calculator.subtract(10,5)).toEqaul(5);
+  expect(calculator.divide(20,2)).toEqaul(10);
+  expect(calculator.multiply(4,4)).toEqaul(16);
+ });
