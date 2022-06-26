ffe0d6f3009ebc3f0a50974cfda303b4655c861e
Matt <97047080+mattxmade@users.noreply.github.com>
2022-06-26 13:43:19 +0100

    Change object function name

--- 03-calculator/calculator.test.js
+++ 03-calculator/calculator.test.js
@@ -1,8 +1,9 @@
 const calculator = require('./calculator.js');
 
 test('Given two numbers, calculate correct number', () => { 
-  expect(calculator.add(1,2)).toEqaul(3);
-  expect(calculator.subtract(10,5)).toEqaul(5);
-  expect(calculator.divide(20,2)).toEqaul(10);
-  expect(calculator.multiply(4,4)).toEqaul(16);
+  expect(calculator.add(1,2)).toEqual(3);
+  expect(calculator.sub(10,5)).toEqual(5);
+  expect(calculator.div(20,2)).toEqual(10);
+  expect(calculator.mul(4,4)).toEqual(16);
  });
+ 
