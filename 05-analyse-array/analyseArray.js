2f29381e10daec5e7375b52a94bdad65e54fda23
Matt <97047080+mattxmade@users.noreply.github.com>
2022-06-27 22:22:52 +0100

    Implement analyseArray funtion

--- 05-analyse-array/analyseArray.js
+++ 05-analyse-array/analyseArray.js
@@ -1,3 +1,23 @@
-function analyseArray(array) {}
+function analyseArray(array) {
+  // guard clauses
+  if (!array.length) return "Array is empty";
+
+  for (const element of array)
+    if (isNaN(element)) return "Array must contain only numbers";
+
+  // retain data without mutation | Pure Function
+  const sortArray = array.sort((a, z) => a - z);
+
+  // return new object
+  return {
+    average: sortArray[sortArray.length - 1] / 2,
+    min: sortArray[0],
+    max: sortArray[sortArray.length - 1],
+    length: sortArray.length,
+  };
+}
+
+const object = analyseArray([1, 8, 3, 4, 2, 6]);
+console.log(object);
 
 module.exports = analyseArray;
