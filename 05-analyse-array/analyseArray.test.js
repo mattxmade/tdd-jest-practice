bcade6a4218856c45883183d3eeb232fb6d5a5bd
Matt <97047080+mattxmade@users.noreply.github.com>
2022-06-27 22:22:06 +0100

    Expand unit test suite for analyseArray function

--- 05-analyse-array/analyseArray.test.js
+++ 05-analyse-array/analyseArray.test.js
@@ -9,3 +9,13 @@ test("Return an object with sort properties", () => {
     length: 6,
   });
 });
+
+test("Return error message when given array is empty", () => {
+  const object = analyseArray([]);
+  expect(object).toBe("Array is empty");
+});
+
+test("Return error message when item of array is not a number", () => {
+  const object = analyseArray(["I", "VIII", "III", "IV", "III", "VI"]);
+  expect(object).toBe("Array must contain only numbers");
+});
